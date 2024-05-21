d684872e57829c04e9b9f32f789137f353283aae
Usame <ussama14863@gmail.com>
2024-05-21 09:51:21 +0300

    create flatlist

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,16 +1,22 @@
-import { Button,View,Text } from "react-native";
+import { Button,View,Text, FlatList } from "react-native";
 import React from 'react';
 import data from "../data.json"
 
 export default function HomeScreen({navigation}: any)
 {
+
+  
 console.log("render home");
 return(  
     <View>
-       <Text> hello its my home screen</Text>
-      <Text>{JSON.stringify(data)}</Text>
-    </View>
-
+    
+      <FlatList data={data}
+        renderItem={({item}) =>
+          <View>
+            <Text>{item.name}</Text>
+          </View>
+      }
+      />
+    </View> 
     )
-
 }
