2f45cc3a4dcce909a9123329a26c8dcfe502b13c
Usame <ussama14863@gmail.com>
2024-05-28 15:52:05 +0300

    create workout item func for render item

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,24 +2,17 @@ import { Button,View,Text, FlatList } from "react-native";
 import React from 'react';
 import data from "../data.json"
 import { Workout } from "../types/data";
+import WorkoutItem from "../components/WorkoutItem";
 
 export default function HomeScreen({navigation}: any)
 {
 
-      const renderItem = ({item}:{item: Workout}) =>
-        (
-          <View>  
-          <Text>{item.name}</Text>
-          <Text>{item.slug}</Text>
-
-        </View>
-        )
-  
 return(  
     <View>
     
       <FlatList data={data as Array<Workout>}
-        renderItem={renderItem}
+        renderItem={WorkoutItem}
+        
       />
     </View> 
     )
